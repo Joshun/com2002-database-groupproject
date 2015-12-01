46ede25859066c49a6ddbfb8522821c809553245
Joshua O'Leary <joshua.oleary@btinternet.com>
2015-12-01 09:57:21 +0000

    Add Calendar class

---
+++ src/com2002/team021/gui/Calendar.java
@@ -0,0 +1,24 @@
+package com2002.team021.gui;
+
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Calendar.java
+ */
+
+public class Calendar extends JFrame {
+    private Date week_beginning;
+    private Date[][] month_days;
+
+    private class ButtonHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+        }
+    }
+
+
+
+}
