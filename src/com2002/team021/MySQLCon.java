42f9c792119651f854ba2cb69e46972ae40ea2bb
Rob Ede <robjtede@gmail.com>
2015-12-02 01:19:16 +0000

    Updated MySQLCon sample to display full patients table Removed TestClass

--- src/com2002/team021/MySQLCon.java
+++ src/com2002/team021/MySQLCon.java
@@ -1,25 +1,39 @@
 package com2002.team021;
 
+import com2002.team021.config.SQL;
+
 import java.sql.*;
-import com2002.team021.config.*;
 
 class MySQLCon {
-    public static void main(String args[]){
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-
-            Connection con=DriverManager.getConnection(com2002.team021.config.SQL.DB_CONNECTION_STRING);
-//here sonoo is the database name, root is the username and root is the password
-            Statement stmt=con.createStatement();
-
-            ResultSet rs=stmt.executeQuery("select * from emp");
-
-            while(rs.next())
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
-
-            con.close();
-
-        }catch(Exception e){ System.out.println(e);}
-
-    }
-}
+	
+	public static void main(String args[]) {
+		
+		try {
+			// Class.forName("com.mysql.jdbc.Driver");
+			
+			Connection con = DriverManager.getConnection(SQL.DB_URL);
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM patients;");
+			
+			while (rs.next()) {
+				System.out.println(rs.getObject(1) + " " +
+					rs.getObject(2) + "\t" +
+					rs.getObject(3) + "\t" + 
+					rs.getObject(4) + "\t" +
+					rs.getObject(5) + "\t" +
+					rs.getObject(6) + "\t" +
+					rs.getObject(7) + "\t" +
+					rs.getObject(8)
+				);
+			}
+			
+			con.close();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+			
+		}// trycat
+		
+	}// main
+	
+}// class
