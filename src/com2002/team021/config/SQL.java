79e33dcc9527508b537f0a70fce36146e5cdace8
Rob Ede <robjtede@gmail.com>
2015-12-02 22:20:20 +0000

    Updated MySQLCon example and SQL constants

--- src/com2002/team021/config/SQL.java
+++ src/com2002/team021/config/SQL.java
@@ -2,13 +2,13 @@ package com2002.team021.config;
 
 public final class SQL {
 	
-	public static final String DB_HOST = "stusql.dcs.shef.ac.uk";
+	public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+	public static final String DB_NAME = "team021";
 	public static final String DB_USER = "team021";
 	public static final String DB_PASS = "c2832352";
-	public static final String DB_NAME = "team021";
+	public static final String DB_HOST = "jdbc:mysql://stusql.dcs.shef.ac.uk/" + DB_NAME;
 	
-	public static final String DB_URL =
-		"jdbc:mysql://" + DB_HOST + "/" + DB_NAME + "?user=" + DB_USER + "&password=" + DB_PASS;
+	public static final String DB_URL = DB_HOST +  "?user=" + DB_USER + "&password=" + DB_PASS;
 	
 	private SQL () {
 		throw new AssertionError();
