2661d4cf8cfb45cfafdcdabf40f9b8257af56732
Joshua O'Leary <joshua.oleary@btinternet.com>
2015-12-03 17:51:54 +0000

    Add LogTreatment

---
+++ src/com2002/team021/LogTreatment.java
@@ -0,0 +1,44 @@
+package com2002.team021;
+
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * LogTreatment.java
+ */
+public class LogTreatment extends JFrame {
+
+    private JPanel appointmentListingPane;
+
+    private class AddTreatmentButtonHandler implements ActionListener {
+        public void actionPerformed(ActionEvent actionEvent) {
+            appointmentListingPane.add(new JLabel("New treatment!"));
+            validate();
+            System.out.println("added");
+        }
+    }
+
+    public LogTreatment() {
+        setTitle("Log treatment");
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new GridLayout(3, 1));
+
+        JLabel appointmentLabel = new JLabel("Add treatments for appointment <appointment>");
+        appointmentListingPane = new JPanel();
+        appointmentListingPane.setLayout(new BoxLayout(appointmentListingPane, BoxLayout.PAGE_AXIS));
+        JButton addTreatmentButton = new JButton("***Add treatment***");
+        addTreatmentButton.addActionListener(new AddTreatmentButtonHandler());
+        contentPane.add(appointmentLabel);
+        contentPane.add(appointmentListingPane);
+        contentPane.add(addTreatmentButton);
+        pack();
+    }
+
+    public static void main(String[] args) {
+        LogTreatment lt = new LogTreatment();
+        lt.setVisible(true);
+    }
+}
