aa125acb6c8a5c6cd92c4e59fafcc047b3dcfaf2
Joshua O'Leary <joshua.oleary@btinternet.com>
2015-12-04 21:14:20 +0000

    Add listeners for LogTreatmentEntry

--- src/com2002/team021/gui/LogTreatmentEntry.java
+++ src/com2002/team021/gui/LogTreatmentEntry.java
@@ -4,12 +4,29 @@ import com2002.team021.Treatment;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 /**
  * Created by joshua on 03/12/15.
  */
 public class LogTreatmentEntry extends JFrame {
+    private Treatment selectedTreatment;
+    private Treatment selectedTreatmentQuantity;
+
+    private JComboBox<Treatment> treatmentSelector;
+    private JSpinner treatmentCounter;
+    private SpinnerNumberModel spinnerModel;
+
+    class TreatmentEntryHandler implements ActionListener {
+        public void actionPerformed(ActionEvent actionEvent) {
+            Treatment treatment = (Treatment) treatmentSelector.getSelectedItem();
+            int quantity = spinnerModel.getNumber().intValue();
+            System.out.println("Adding treatment \"" + treatment + "\" with quantity " + quantity);
+        }
+    }
+
     public LogTreatmentEntry(Treatment[] treatments) {
         Container contentPane = getContentPane();
         contentPane.setLayout(new GridLayout(3, 2));
@@ -23,15 +40,15 @@ public class LogTreatmentEntry extends JFrame {
 //            treatmentList.add(treatmentString);
 //        }
 
-
-        JComboBox<Treatment> treatmentSelector = new JComboBox<>(treatments);
+        treatmentSelector = new JComboBox<>(treatments);
         contentPane.add(treatmentSelector);
 
-        SpinnerNumberModel spinnerModel = new SpinnerNumberModel(1, 1, null, 1);
-        JSpinner treatmentCount = new JSpinner(spinnerModel);
-        contentPane.add(treatmentCount);
+        spinnerModel = new SpinnerNumberModel(1, 1, null, 1);
+        treatmentCounter = new JSpinner(spinnerModel);
+        contentPane.add(treatmentCounter);
 
         JButton addTreatmentButton = new JButton("Add treatment");
+        addTreatmentButton.addActionListener(new TreatmentEntryHandler());
         contentPane.add(new JLabel());
         contentPane.add(addTreatmentButton);
 
