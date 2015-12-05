2a77276cb65b3624af7b08fc84b73b009e208440
Rob Ede <robjtede@gmail.com>
2015-12-05 21:51:12 +0000

    Added DentistPractice class to use as startup class

---
+++ src/com2002/team021/DentistPractice.java
@@ -0,0 +1,13 @@
+package com2002.team021;
+
+public class DentistPractice {
+	
+	public DentistPractice () {
+		
+	}
+	
+	public static void main (String args[]) {
+		System.out.println("Main class");
+	}
+	
+}
