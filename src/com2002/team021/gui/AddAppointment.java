1942d36f9284b633f5c9bbde26ed43e62391e7d6
Joshua O'Leary <joshua.oleary@btinternet.com>
2015-12-07 17:54:42 +0000

    Implement AddAppointment

--- src/com2002/team021/gui/AddAppointment.java
+++ src/com2002/team021/gui/AddAppointment.java
@@ -5,19 +5,33 @@ import com2002.team021.Practitioner;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by joshua on 07/12/15.
  */
+
+
 public class AddAppointment extends JFrame {
     private JComboBox<Patient> patientJComboBox;
     private JComboBox<Practitioner> practitionerJComboBox;
     private JComboBox<String> appointmentJComboBox;
-    private String[] appointmentTypes = { "Checkup", "Hygienist", "Dentist" };
+    private String[] appointmentTypes = { "Checkup", "Hygienist", "Treatment" };
+
+    public class AddAppointmentButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent actionEvent) {
+            Patient patient = (Patient) patientJComboBox.getSelectedItem();
+            Practitioner practitioner = (Practitioner) practitionerJComboBox.getSelectedItem();
+            String appointmentType = (String) appointmentJComboBox.getSelectedItem();
+
+            System.out.println(patient.toString() + practitioner.toString() + appointmentType);
+        }
+    }
 
     public AddAppointment(Patient[] allPatients, Practitioner[] allPractitioners) {
         Container contentPane = getContentPane();
-        contentPane.setLayout(new GridLayout(3, 2));
+        contentPane.setLayout(new GridLayout(4, 2));
 
         contentPane.add(new JLabel("Patient"));
         patientJComboBox = new JComboBox<>(allPatients);
@@ -29,10 +43,21 @@ public class AddAppointment extends JFrame {
 
         contentPane.add(new JLabel("Type"));
         appointmentJComboBox = new JComboBox<>(appointmentTypes);
+        contentPane.add(appointmentJComboBox);
+
+        contentPane.add(new JLabel());
+        JButton addAppointmentButton = new JButton("Add appointment");
+        addAppointmentButton.addActionListener(new AddAppointmentButtonListener());
+        contentPane.add(addAppointmentButton);
+
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
     public static void main(String[] args) throws java.sql.SQLException {
-        Patient[] patients = { new Patient("a", "b", 1, 1, "1", "1", "1") };
+        Patient[] patients = { new Patient("", "b", 1, 1, "14", "st74hr", null), new Patient("", "c", 1, 1, "14", "st74hr", null) };
+        Practitioner[] practitioners = { new Practitioner("Mr a", "Dentist") };
+        AddAppointment ap = new AddAppointment(patients, practitioners);
     }
 }
