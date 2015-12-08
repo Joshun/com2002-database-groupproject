8bae9d1cdd29be2e65be75bde2152904c754ccd1
Joshua O'Leary <joshua.oleary@btinternet.com>
2015-12-08 22:46:41 +0000

    Add computeEndTime

--- src/com2002/team021/gui/AddAppointment.java
+++ src/com2002/team021/gui/AddAppointment.java
@@ -8,6 +8,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by joshua on 07/12/15.
@@ -18,7 +20,12 @@ public class AddAppointment extends JFrame {
     private JComboBox<Patient> patientJComboBox;
     private JComboBox<Practitioner> practitionerJComboBox;
     private JComboBox<String> appointmentJComboBox;
+    private SpinnerNumberModel startHourEntryModel;
+    private SpinnerNumberModel startMinuteEntryModel;
+    private JLabel endTimeLabel;
+
     private String[] appointmentTypes = { "Checkup", "Hygienist", "Treatment" };
+    private int[] appointmentDurations = { 20, 20, 60 };
 
     public class AddAppointmentButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent actionEvent) {
@@ -32,7 +39,7 @@ public class AddAppointment extends JFrame {
 
     public AddAppointment(ArrayList<Patient> allPatients, ArrayList<Practitioner> allPractitioners) {
         Container contentPane = getContentPane();
-        contentPane.setLayout(new GridLayout(4, 2));
+        contentPane.setLayout(new GridLayout(6, 2));
 
         contentPane.add(new JLabel("Patient"));
         patientJComboBox = new JComboBox<>();
@@ -53,6 +60,22 @@ public class AddAppointment extends JFrame {
         appointmentJComboBox = new JComboBox<>(appointmentTypes);
         contentPane.add(appointmentJComboBox);
 
+        JPanel timeContainer = new JPanel(new GridLayout(2, 2));
+        timeContainer.add(new JLabel("HH"));
+        timeContainer.add(new JLabel("MM"));
+        startHourEntryModel = new SpinnerNumberModel(9, 9, 16, 1);
+        startMinuteEntryModel = new SpinnerNumberModel(0, 0, 59, 1);
+        JSpinner startHourEntry = new JSpinner(startHourEntryModel);
+        JSpinner startMinuteEntry = new JSpinner(startMinuteEntryModel);
+        timeContainer.add(startHourEntry);
+        timeContainer.add(startMinuteEntry);
+        contentPane.add(new JLabel("Start time"));
+        contentPane.add(timeContainer);
+
+        contentPane.add(new JLabel("End time"));
+        endTimeLabel = new JLabel("-");
+        contentPane.add(endTimeLabel);
+
         contentPane.add(new JLabel());
         JButton addAppointmentButton = new JButton("Add appointment");
         addAppointmentButton.addActionListener(new AddAppointmentButtonListener());
@@ -61,6 +84,21 @@ public class AddAppointment extends JFrame {
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    private Date computeEndTime(Date startTime, String appointmentType) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(startTime);
+        switch (appointmentType) {
+            case "Checkup":
+            case "Hygienist":
+                cal.add(Calendar.MINUTE, 20);
+                break;
+            case "Treatment":
+                cal.add(Calendar.MINUTE, 59);
+                break;
+        }
+        return cal.getTime();
     }
 
     public static void main(String[] args) throws java.sql.SQLException {
